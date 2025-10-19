d8bbc684c7c11d5222b7807d147270ae8008450c
sahilpisal220 <sahilpisal1703@gmail.com>
2025-10-19 16:20:28 +0530

    Completed the UI of the Home Route

--- src/landing_page/home/Awards.jsx
+++ src/landing_page/home/Awards.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
 
 const Awards = () => {
   return (
-    <div>Awards</div>
-  )
-}
+    <div className="container">
+      <div className="row">
+        <div className="col-6 p-5 border border-2">
+          <img src="media/image/largestBroker.svg" alt="lagest-broker image" />
+        </div>
 
-export default Awards
+        <div className="col-6 p-5 border border-2 my-3">
+          <h1 className="fs-2">Largest stock broker in India</h1>
+          <p className="mb-5 text-muted">
+            2+ million Zerodha clients contribute to over 15% of all retail
+            order volumes in India daily by trading and investing in:
+          </p>
+
+          <div className="row">
+            <div className="col-6">
+              <ul>
+                <li>
+                  <p>Futures and Options</p>
+                </li>
+                <li>
+                  <p>Commodity derivatives</p>
+                </li>
+                <li>
+                  <p>Currency derivatives</p>
+                </li>
+              </ul>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>
+                  <p>Stocks & IPOs</p>
+                </li>
+                <li>
+                  <p>Direct mutual funds</p>
+                </li>
+                <li>
+                  <p>Bonds and Govt. Securities</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <img className="w-100" src="media/image/pressLogos.png" alt="Press-Logo" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Awards;
--- src/landing_page/home/Pricing.jsx
+++ src/landing_page/home/Pricing.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
 
 const Pricing = () => {
   return (
-    <div>Pricing</div>
-  )
-}
+    <div className="container">
+      <div className="row">
+        <div className="col-5 border p-5">
+          <h1 className='fs-2 "mb-3'>Unbeatable pricing</h1>
+          <p className="text-muted">
+            We pioneered the concept of discount broking and price transparency
+            in India. Flat fees and no hidden charges.
+          </p>
 
-export default Pricing
+          <a href="" className="text-decoration-none">
+            See Pricing <i class="fa-solid fa-arrow-right"></i>
+          </a>
+        </div>
+        <div className="col-1"></div>
+        <div className="col-6 border">
+          
+          <div className="row text-center border p-5">
+            <div className="col border p-3">
+              <h1 className="mb-3 ">₹0</h1>
+              <p>
+                Free equity delivery and
+                <br />
+                direct mutual funds
+              </p>
+            </div>
+            <div className="col border p-3">
+              <h1 className="mb-3">₹20</h1>
+              <p>Intraday and F&O</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pricing;
